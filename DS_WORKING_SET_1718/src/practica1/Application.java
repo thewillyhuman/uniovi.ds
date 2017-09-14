68cd26f425937608585f65e09b2f2eadd19bcc05
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-09-14 14:29:14 +0200

    Added 2728 working set

---
+++ DS_WORKING_SET_1718/src/practica1/Application.java
@@ -0,0 +1,69 @@
+package practica1;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		double[] temperaturas = new double[100];
+		Temperature[] temperatures = new Temperature[100];
+
+		// New data input
+		for (int i = 0; i < temperaturas.length; i++)
+			temperatures[i] = new Temperature(leeSensorCelsius());
+
+		// Calculations with data in celsius.
+		double mCelsius = 0;
+		for (int i = 0; i < temperatures.length; i++)
+			mCelsius += temperatures[i].asCelsius();
+		mCelsius = mCelsius / temperatures.length;
+		System.out.printf("La media en Celsius es: %.1f ºC\n", mCelsius);
+
+		// Calculation with data in Farenheit
+		double mFahrenheit = 0;
+		for (int i = 0; i < temperatures.length; i++)
+			mFahrenheit += temperatures[i].asFarenheit();
+		mFahrenheit = mFahrenheit / temperatures.length;
+		System.out.printf("La media en Fahrenheit es: %.1f ºF\n", mFahrenheit);
+
+		// Another calculation with data in Fahrenheit
+		double nVarianza = 0;
+		for (int i = 0; i < temperatures.length; i++)
+			nVarianza += Math.pow((temperatures[i].asFarenheit()) - mFahrenheit,
+					2);
+		nVarianza = nVarianza / temperatures.length;
+		System.out.printf("La varianza en Fahrenheit es: %.1f\n", nVarianza);
+
+		// *** OLD WAY ***
+
+		// Toma de datos
+		for (int i = 0; i < temperaturas.length; i++)
+			temperaturas[i] = leeSensorCelsius();
+
+		// Cálculo con los datos en Celsius
+		double mediaCelsius = 0;
+		for (int i = 0; i < temperaturas.length; i++)
+			mediaCelsius += temperaturas[i];
+		mediaCelsius = mediaCelsius / temperaturas.length;
+		System.out.printf("La media en Celsius es: %.1f ºC\n", mediaCelsius);
+
+		// Cálculo con los datos en Fahrenheit
+		double mediaFahrenheit = 0;
+		for (int i = 0; i < temperaturas.length; i++)
+			mediaFahrenheit += temperaturas[i] * 9 / 5 + 32;
+		mediaFahrenheit = mediaFahrenheit / temperaturas.length;
+		System.out.printf("La media en Fahrenheit es: %.1f ºF\n",
+				mediaFahrenheit);
+
+		// Otro cálculo con los datos en Fahrenheit
+		double varianza = 0;
+		for (int i = 0; i < temperaturas.length; i++)
+			varianza += Math
+					.pow((temperaturas[i] * 9 / 5 + 32) - mediaFahrenheit, 2);
+		varianza = varianza / temperaturas.length;
+		System.out.printf("La varianza en Fahrenheit es: %.1f\n", varianza);
+	}
+
+	public static double leeSensorCelsius() {
+		return Math.random() * 30;
+	}
+}
---
+++ DS_WORKING_SET_1718/src/practica1/Temperature.java
@@ -0,0 +1,57 @@
+package practica1;
+
+public class Temperature {
+
+	/**
+	 * Temperature object storing the celsius. farenheit it in both units.
+	 * 
+	 * @version 201709140927
+	 * @author Guillermo Facundo Colunga
+	 */
+
+	private double celsius;
+
+	/**
+	 * 
+	 * @param celsius the initial temperature in celsius.
+	 */
+	public Temperature(double celsius) {
+		setCelsius(celsius);
+	}
+
+	/**
+	 * Devuelve el valor de la temperatura en celsius.
+	 * 
+	 * @return el valor de la temperatura en celsius.
+	 */
+	public double asCelsius() {
+		return this.celsius;
+	}
+
+	/**
+	 * Cambia el valor de la temperatura en celsius.
+	 * 
+	 * @param celsius son los nuevos grados a establecer como temperatura.
+	 */
+	public void setCelsius(double celsius) {
+		this.celsius = celsius;
+	}
+
+	/**
+	 * Devuelve el valor de la temperatura en farenheit.
+	 * 
+	 * @return el valor de la temperatura en farenheit.
+	 */
+	public double asFarenheit() {
+		return (this.celsius * 9 / 5) + 32;
+	}
+
+	/**
+	 * Cambia el valor de la temperatura a farenheit.
+	 * 
+	 * @param farenheit son los grados a establecer como temperatura.
+	 */
+	public void setFarenheit(double farenheit) {
+		this.celsius = (farenheit - 32) * 5 / 9;
+	}
+}
