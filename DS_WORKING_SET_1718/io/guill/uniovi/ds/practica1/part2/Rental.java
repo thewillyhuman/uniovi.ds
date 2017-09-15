fa9ec8a42f800d33a7c1448ad8257e98836bd5f4
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-09-15 14:38:04 +0200

    Minor Changes

--- DS_WORKING_SET_1718/io/guill/uniovi/ds/practica1/part2/Rental.java
+++ DS_WORKING_SET_1718/io/guill/uniovi/ds/practica1/part2/Rental.java
@@ -1,10 +1,31 @@
 package io.guill.uniovi.ds.practica1.part2;
 
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Mike Taghavi (mitghi[at]me.com)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 /**
  * Rental.java
  *
  * @author willy
- * @version 201709151425
+ * @version 201709151437
  * @since 15/09/2017
  * @formatter Oviedo Computing Community
  */
